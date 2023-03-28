ae0345db6289c50da0cb2888cb62292dd1c21485
Kevin Gemolo <kevingemolo@gmail.com>
2023-03-28 13:56:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let lista = ""
+let arrEl = ['acqua', 'miele', 'caffe', 'pesce']
+let listaDOM = null
 
 function setListaTagLI(arr){
 	for(let i = 0; i < arr.length; i++){
-		
+		var li = document.createElement('li')
+		listaDOM.appendChild(li)
+		li.innerHTML = arr[i]
 	}
+}
+
+function add(){
+	let input = document.getElementById("addInput").value
+	arrEl.push(input)
+	var li = document.createElement('li')
+	listaDOM.appendChild(li)
+	li.innerHTML = input
+	document.getElementById("addInput").value = ""
 }
 
 
 window.onload = function() {
-	debugger
+	listaDOM = document.getElementById("lista")
+	setListaTagLI(arrEl)	
 }
